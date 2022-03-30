e3207118971ad0eb96df58e044a9bda1bbc897a6
xenturion111 <rezaaditiyam@gmail.com>
2022-03-30 14:10:30 +0700

    berhasil di commit

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -11,12 +11,12 @@ const SideNav = () => {
             <Link className="logo" to='/'> 
                 <img id="profile" src={profile} alt="Profile"/>
             </Link>
-            <Link  activeClassName="active" exact="true" to="/"></Link> 
-            <Link  activeClassName="active" className="about-link" exact="true" to="/About">About</Link> 
-            <Link  activeClassName="active" className="experience-link" exact="true" to="/Experience">Experiences</Link>
-            <Link  activeClassName="active" className="skills-link" exact="true" to="/Skills">Skills</Link>
-            <Link  activeClassName="active" className="interest-link" exact="true" to="/Interest">Interest</Link>
-            <Link  activeClassName="active" className="awards-link" exact="true" to="/Awards">Awards</Link>
+            <Link  to="/"></Link> 
+            <Link  to="/About">About</Link> 
+            <Link  to="/Experience">Experiences</Link>
+            <Link  to="/Skills">Skills</Link>
+            <Link  to="/Interest">Interest</Link>
+            <Link  to="/Awards">Awards</Link>
         </div>
     );
 };
